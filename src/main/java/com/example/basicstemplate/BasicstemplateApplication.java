427ab40f198a6c917a12653daef9e2dfb6fdd588
ljl000 <1524275389@qq.com>
2020-03-31 15:28:30 +0800

    第一次提交基础接口项目

---
+++ src/main/java/com/example/basicstemplate/BasicstemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.basicstemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicstemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicstemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/basicstemplate/BasicstemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicstemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicstemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
