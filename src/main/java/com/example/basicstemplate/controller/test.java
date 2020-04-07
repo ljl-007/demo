ba599a094e2bd4956d6436fb020d7843c1a65eb7
ljl000 <1524275389@qq.com>
2020-04-07 14:39:21 +0800

    新增index

--- src/main/java/com/example/basicstemplate/controller/test.java
+++ src/main/java/com/example/basicstemplate/controller/test.java
@@ -2,9 +2,8 @@ package com.example.basicstemplate.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class test {
